1b44fcfb30b2db99c8b63d915b88bcb5492b7264
NickolasH418 <nickolas.harveybo695@students.cmcss.net>
2026-02-19 08:33:10 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,29 @@
+from data import a, b, c, d, e, f
+
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+a = float(a)
+print(a)
+
+# b
+print(b)
+
+# c
+c = int(c)
+print(c)
+
+# d
+d = int(d)
+print(d)
+
+# e
+print(e)
+
+# f
+f = float(f)
+print(f)
+
 
